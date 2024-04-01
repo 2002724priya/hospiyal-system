587b131d18a004111be7b751f20b0c49f77cba16
ITRC <ITRC@SL040201>
2024-04-01 10:04:28 +0530

    nurse

---
+++ doctors.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class Class1
+{
+	public string Doctor name{get; set }
+     public string Doctor field {get;set}
+	  public string Doctor age {get; set}
+	public string Doctors visit {get;set}
+	public string Doctoes salary{get;set}
+	public Class1()
+	{
+	}
+}
